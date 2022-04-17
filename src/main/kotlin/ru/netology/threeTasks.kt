9aaa95ad0c1244142baf8a944e4dc38164ecfdee
Сергей Акимов <akimovserega@gmail.com>
2022-04-17 23:17:13 +0500

    task 1 is done

--- src/main/kotlin/ru/netology/threeTasks.kt
+++ src/main/kotlin/ru/netology/threeTasks.kt
@@ -2,4 +2,12 @@ package ru.netology
 
 fun main() {
 
+    val feeRate: Float = 0.75F / 100
+    val minFee: Int = 35_00
+    val amount: Int = 5_250_00
+    val fee: Int = if (amount * feeRate > minFee) (amount * feeRate).toInt() else minFee
+
+    println("Комиссия составила ${fee / 100} руб. ${fee % 100} коп.")
+
+
 }
