5e8f884523047bddbab525f43b916f961d062596
Сергей Акимов <akimovserega@gmail.com>
2022-04-19 22:50:55 +0500

    task 3 is done

--- src/main/kotlin/ru/netology/threeTasks.kt
+++ src/main/kotlin/ru/netology/threeTasks.kt
@@ -2,15 +2,59 @@ package ru.netology
 
 fun main() {
 
-    val likes: Int = 1101
+    var currentPrice: Int = 1_012_00
+    var musicFan: Boolean = true
+    val discountFan: Float = (1.0f / 100)
+    val discount: Float = (5.0f / 100)
+    val discountFix: Int = 100_00
 
-    // если последняя цифра 1, но не 11, то "человеку"
-    // если 0
-    if (likes < 1) {
-        println("У Вас нет лайков")
-    } else {
-        println("Понравилось ${if (likes % 10 == 1 && likes % 100 != 11) "$likes человеку" else "$likes людям"}")
+    val priceMin: Int = 1_000_00
+    val priceMax: Int = 10_000_00
+    val pricePlus: Int = 1_00
+    var discountPrice: Int = 0
+
+
+    if (currentPrice < priceMin + pricePlus) {
+        discountPrice = currentPrice;
+
+    } else if (currentPrice >= priceMin + pricePlus && currentPrice < priceMax + pricePlus) {
+        discountPrice = currentPrice - discountFix;
+
+    } else if (currentPrice >= priceMax + pricePlus) {
+        discountPrice = (currentPrice * (1 - discount)).toInt()
     }
+
+    if (musicFan) {
+        discountPrice = (discountPrice * (1 - discountFan)).toInt()
+    }
+
+    println("Сумма покупок ${currentPrice / 100} руб. ${currentPrice % 100} коп.")
+    println("Сумма с учетом скидок  ${discountPrice / 100} руб. ${discountPrice % 100} коп.")
+    println("Ваша скидка составила ${(currentPrice - discountPrice) / 100} руб. ${(currentPrice - discountPrice) % 100} коп.")
+
+
+    // другой вариант: сумма больше, а статуса "меломана" нет
+    currentPrice = 10_200_00
+    musicFan = false
+
+
+    if (currentPrice < priceMin + pricePlus) {
+        discountPrice = currentPrice;
+
+    } else if (currentPrice >= priceMin + pricePlus && currentPrice < priceMax + pricePlus) {
+        discountPrice = currentPrice - discountFix;
+
+    } else if (currentPrice >= priceMax + pricePlus) {
+        discountPrice = (currentPrice * (1 - discount)).toInt()
+    }
+
+    if (musicFan) {
+        discountPrice = (discountPrice * (1 - discountFan)).toInt()
+    }
+
+    println("\n\nСумма покупок ${currentPrice / 100} руб. ${currentPrice % 100} коп.")
+    println("Сумма с учетом скидок  ${discountPrice / 100} руб. ${discountPrice % 100} коп.")
+    println("Ваша скидка составила ${(currentPrice - discountPrice) / 100} руб. ${(currentPrice - discountPrice) % 100} коп.")
 
 
 }
